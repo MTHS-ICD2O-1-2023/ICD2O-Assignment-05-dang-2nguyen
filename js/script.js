ebc5c1da49999335506f4ca8869f9b691cbc02a5
Hoang Thien Dang Nguyen <159185239+dang-2nguyen@users.noreply.github.com>
2024-05-26 18:55:48 +0000

    sfsvsvs

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Dang All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Dang
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+function checking() {
+  let fibArray = [0,1]
+  const number = parseInt(document.getElementById("number").value)
+
+  for (let counter = 2; counter < number; counter++) {
+    fibArray[counter] = fibArray[counter - 1] + fibArray[counter - 2]
+  }
+  document.getElementById("result").innerHTML = fibArray
+}
